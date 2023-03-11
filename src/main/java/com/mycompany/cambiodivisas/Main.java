b413e6948d5f3a4049c1972e739be7579f0f3802
Lucas <lkezequiel@gmail.com>
2023-03-10 21:26:51 -0300

    inicio de proyecto

---
+++ src/main/java/com/mycompany/cambiodivisas/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cambiodivisas;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author lkeze
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Pantalla panta = new Pantalla();
+    panta.setVisible(true);
+    panta.setLocationRelativeTo(null);
+    
+    
+    
+    
+        }
+    }
---
+++ src/main/java/igu/Intercambio.java
@@ -0,0 +1,45 @@
+
+package igu;
+
+public class Intercambio {
+    private double valor, resultado;
+    private int divInit, divFin;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getDivInit() {
+        return divInit;
+    }
+
+    public void setDivInit(int divInit) {
+        this.divInit = divInit;
+    }
+
+    public int getDivFin() {
+        return divFin;
+    }
+
+    public void setDivFin(int divFin) {
+        this.divFin = divFin;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public Intercambio() {
+    
+    }
+    
+    
+}
